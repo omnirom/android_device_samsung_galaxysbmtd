6e4bcfb61f29e4cb4f99358698dfd91141552dd0
Humberto Borba <humberos@gmail.com>
2014-01-05 00:51:40 -0200

    galaxysb: remove duplicated inherit
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I42c9ac974b1a7f386cdcbaa46086179e47ffbf90

--- omni_galaxysbmtd.mk
+++ omni_galaxysbmtd.mk
@@ -15,9 +15,6 @@
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit Omni GSM telephony parts
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
